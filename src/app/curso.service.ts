a75c9e7e245f0a6360a8c8552d9e76179132f94d
Hygor <Hygor@DESKTOP-55BA59J>
2023-11-28 22:22:28 -0300

    metodos http

--- src/app/curso.service.ts
+++ src/app/curso.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CursoService {
+  private apiUrl = 'http://localhost:3000/api/cursos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCursos(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getCurso(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  salvarCurso(curso: any): Observable<any> {
+    if (curso.id) {
+      return this.http.put<any>(`${this.apiUrl}/${curso.id}`, curso);
+    } else {
+      return this.http.post<any>(this.apiUrl, curso);
+    }
+  }
+
+  excluirCurso(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }
