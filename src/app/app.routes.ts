9226203c349a11a58e56450644dcf901f8126f12
Hygor <Hygor@DESKTOP-55BA59J>
2023-11-28 22:10:13 -0300

    implementando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListaComponent } from './lista/lista.component';
+import { CriarEditarComponent } from './criar-editar/criar-editar.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListaComponent },
+  { path: 'criar', component: CriarEditarComponent },
+  { path: 'editar/:id', component: CriarEditarComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
