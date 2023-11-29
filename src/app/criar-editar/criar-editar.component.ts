b1db2f5fea6807be0382e34c33c81db6cbbd23d8
Hygor <Hygor@DESKTOP-55BA59J>
2023-11-28 22:39:07 -0300

    classe para criar e editar um item

--- src/app/criar-editar/criar-editar.component.ts
+++ src/app/criar-editar/criar-editar.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CursoService } from '../curso.service';
 
 @Component({
   selector: 'app-criar-editar',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './criar-editar.component.html',
-  styleUrl: './criar-editar.component.css'
+  styleUrls: ['./criar-editar.component.css'],
 })
-export class CriarEditarComponent {
+export class CriarEditarComponent implements OnInit {
+  curso: any = {};
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cursoService: CursoService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    if (id) {
+      this.cursoService.getCurso(id).subscribe((data: any) => {
+        this.curso = data;
+      });
+    }
+  }
+
+  salvarCurso() {
+    this.cursoService.salvarCurso(this.curso).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
