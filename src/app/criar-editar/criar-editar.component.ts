43dceb9b50891362c43df067e7fa90f9124145f8
Hygor <Hygor@DESKTOP-55BA59J>
2023-11-28 21:56:07 -0300

    componente "criar-editar"

---
+++ src/app/criar-editar/criar-editar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-criar-editar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './criar-editar.component.html',
+  styleUrl: './criar-editar.component.css'
+})
+export class CriarEditarComponent {
+
+}
